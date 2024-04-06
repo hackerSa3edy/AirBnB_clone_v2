50e813a459dadcd13079283c73e9c3082e0f1ca9
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-04-06 15:51:55 +0200

    Add script to clean up old versions of deployed web_static directories

---
+++ 100-clean_web_static.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""
+This script is used to clean up old versions of deployed web_static directories
+
+It uses Fabric, a Python library and command-line tool for streamlining the use
+of SSH for application deployment.
+
+The script is configured to clean up on two servers:
+'web-01.s1cario.tech' and 'web-02.s1cario.tech'.
+
+Functions:
+    do_clean: Cleans up old versions of deployed web_static directories.
+"""
+
+from fabric.api import *
+
+env.hosts = ['web-01.s1cario.tech', 'web-02.s1cario.tech']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+
+def do_clean(number=0):
+    """
+    Cleans up old versions of deployed web_static directories.
+
+    The function keeps the number of versions specified by the 'number'
+    parameter and removes the rest. If 'number' is 0 or 1, it keeps only the
+    most recent version. The function cleans up both locally (in the
+    'versions' directory) and on the remote servers (in the
+    '/data/web_static/releases' directory).
+
+    Args:
+        number (int, optional): The number of versions to keep. Defaults to 0.
+
+    Returns:
+        None
+    """
+    if number == 0:
+        number = 1
+
+    with lcd('versions'):
+        local(
+            f"ls -t -1 | tail -n +{number + 1} | "
+            "xargs -I :archive rm -rf :archive"
+            )
+
+    with cd('/data/web_static/releases'):
+        run(
+            f"ls -t -1 | tail -n +{number + 1} | "
+            "xargs -I :archive rm -rf :archive"
+            )
