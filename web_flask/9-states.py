b4f8b731bcf580dec5103cfef002564d970e9678
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-04-22 06:31:23 +0200

    Fix pycodestyle issue

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a Flask web application that starts on 0.0.0.0 on port 5000.
+This module defines a Flask web application that starts on 0.0.0.0
+on port 5000.
 
 Imports:
     Flask: Flask web server framework.
@@ -8,12 +9,16 @@ Imports:
     models: Module that contains the application models.
 
 Routes:
-    /states: Route that displays a HTML page with a list of all State objects in the database.
-    /states/<id>: Route that displays a HTML page with a specific State object and its associated City objects in the database.
+    /states: Route that displays a HTML page with a list of all State objects
+    in the database.
+    /states/<id>: Route that displays a HTML page with a specific State object
+    and its associated City objects in the database.
 
 Functions:
-    clear_session(exception): Function that is called after each request to close the current SQLAlchemy session.
-    cities(id): Function that is called when the /states or /states/<id> route is accessed.
+    clear_session(exception): Function that is called after each request to
+    close the current SQLAlchemy session.
+    cities(id): Function that is called when the /states or /states/<id>
+    route is accessed.
 
 App:
     app: Instance of the Flask class.
@@ -29,10 +34,13 @@ def clear_session(exception):
     """
     Closes the current SQLAlchemy session.
 
-    This function is called after each request, regardless of whether an exception occurred during the request. The exception that occurred during the request, if any, is passed to this function.
+    This function is called after each request, regardless of whether an
+    exception occurred during the request. The exception that occurred during
+    the request, if any, is passed to this function.
 
     Parameters:
-        exception (Exception): The exception that occurred during the request, if any.
+        exception (Exception): The exception that occurred during the request,
+        if any.
 
     Returns: None
     """
@@ -42,15 +50,21 @@ def clear_session(exception):
 @app.route('/states/<id>', strict_slashes=False)
 def cities(id):
     """
-    Renders a HTML page with a list of all State objects or a specific State object and its associated City objects in the database.
+    Renders a HTML page with a list of all State objects or a specific State
+    object and its associated City objects in the database.
 
-    The HTML page is found in the templates folder and is named 9-states.html. The list of State objects or the specific State object and its associated City objects is passed to the template under the variable name data.
+    The HTML page is found in the templates folder and is named 9-states.html.
+    The list of State objects or the specific State object and its associated
+    City objects is passed to the template under the variable name data.
 
     Parameters:
-        id (str): The id of the specific State object to display. If this is None, all State objects are displayed.
+        id (str): The id of the specific State object to display. If this is
+        None, all State objects are displayed.
 
     Returns:
-        render_template: A function that takes in the name of a template file and a variable number of keyword arguments, and returns a string with placeholders in the template file replaced with the appropriate values.
+        render_template: A function that takes in the name of a template file
+        and a variable number of keyword arguments, and returns a string with
+        placeholders in the template file replaced with the appropriate values.
     """
     data = {'states': None, 'state': None}
     if id is not None:
